997ee12129e5a32b2203556d539c95e64e01c67c
Lucas Hautrive <lucahautrive@gmail.com>
2026-02-11 21:59:05 +1100

    Initial Commit.

---
+++ src/main/java/me/ilucah/audiovisualiser_service/AudiovisualiserServiceApplication.java
@@ -0,0 +1,13 @@
+package me.ilucah.audiovisualiser_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiovisualiserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiovisualiserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ilucah/audiovisualiser_service/AudiovisualiserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ilucah.audiovisualiser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiovisualiserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
